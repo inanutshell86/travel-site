03cd7e3a302986504bc351774c8422c1c6cfd8f9
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-05-31 19:54:40 +0300

    Completed build task.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -16,7 +16,7 @@ class StickyHeader {
   }
 
   refreshWaypoints() {
-    this.lazyImages.on("load", funciton() {
+    this.lazyImages.on("load", function() {
       Waypoint.refreshAll();
     });
   }
