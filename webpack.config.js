899800b651c80cd6aaddc23a81981ae0d2b51b91
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-05-31 16:12:01 +0300

    Completed the lazyloading functionality.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 var path = require("path");
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
   output: {
     path: path.resolve(__dirname, "./app/temp/scripts"),
-    filename: "App.js"
-  }
-  /* module: {
-    loaders: [
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
       {
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['es2015']
+          presets: ["es2015"]
         },
         test: /\.js$/,
         exclude: /node_modules/
       }
     ]
-  } */
+  }
 };
